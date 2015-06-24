98f549e120bd4446629c3aa63b225448f91deb54
aorura <hyponus@gmail.com>
2015-06-24 17:05:29 +0900

    add detail activity

---
+++ tv/src/main/java/com/aorura/familyphoto/leanback/PhotoDetailsFragment.java
@@ -0,0 +1,9 @@
+package com.aorura.familyphoto.leanback;
+
+import android.support.v17.leanback.app.DetailsFragment;
+
+/**
+ * Created by chalse.park on 2015-06-24.
+ */
+public class PhotoDetailsFragment extends DetailsFragment {
+}
